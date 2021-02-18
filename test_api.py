1ca85e834951a3d78b16945506440d43da75a81e
Owen McBrien <omcbrien02@qub.ac.uk>
2021-02-18 21:04:39 +0000

    Reading from settings

--- test_api.py
+++ test_api.py
@@ -1,21 +1,41 @@
 import tweepy
 
-def establishAuthAPI(consumer_key, consumer_secret):
+def readSettings():
 
-    auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
+    import yaml
+
+    with open('settings.yaml', 'r') as stream:
+        all_settings = yaml.safe_load(stream)
+
+    api_key = all_settings['api_key']
+    api_secret_key = all_settings['api_secret_key']
+    bearer_token = all_settings['bearer_token']
+    user_id = all_settings['user_id']
+
+    full_parameters = (api_key,
+                        api_secret_key,
+                        bearer_token,
+                        user_id)
+
+    return full_parameters
+
+def establishAuthAPI(api_key, api_secret_key):
+
+    auth = tweepy.AppAuthHandler(api_key, api_secret_key)
     api = tweepy.API(auth)
 
     return auth, api
 
+# ------------------------------------------------------------------------------
+
 def main():
 
-    consumer_key = 'SE9GwvVCgOj2pBFsSlLYoZmTf'
-    consumer_secret = 'JNaLFvySvRbwLAczIvkb9QIAXuDCamixeIaNqxrOkNfCrFduNB'
+    (api_key,
+    api_secret_key,
+    bearer_token,
+    user_id) = readSettings()
 
-    auth, api = establishAuthAPI(consumer_key, consumer_secret)
-
-    print(type(auth))
-    print(type(api))
+    auth, api = establishAuthAPI(api_key, api_secret_key)
 
     for tweet in tweepy.Cursor(api.user_timeline, id = 'ormcbrien').items(20):
         print(tweet.id)
